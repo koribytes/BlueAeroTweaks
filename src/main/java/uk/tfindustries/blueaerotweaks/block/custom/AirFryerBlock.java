18565e4d38244ec473c193a3c159311983983492
Koribytes <supersquids12@gmail.com>
2026-09-12 06:57:34 -0400

    Air Fryer WIP

---
+++ src/main/java/uk/tfindustries/blueaerotweaks/block/custom/AirFryerBlock.java
@@ -0,0 +1,10 @@
+package uk.tfindustries.blueaerotweaks.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class AirFryerBlock extends Block {
+
+    public AirFryerBlock(Properties properties) {
+        super(properties);
+    }
+}
