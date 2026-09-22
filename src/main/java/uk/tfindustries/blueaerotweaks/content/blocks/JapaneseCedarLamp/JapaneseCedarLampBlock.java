5529dac9199e2e978df963f9c56d27dc793e8dd6
Koribytes <supersquids12@gmail.com>
2026-09-22 02:41:16 -0400

    Japanese Cedar Lamp Lighting Functionality You can either right click it to toggle or override with a redstone signal update

--- src/main/java/uk/tfindustries/blueaerotweaks/content/blocks/JapaneseCedarLamp/JapaneseCedarLampBlock.java
+++ src/main/java/uk/tfindustries/blueaerotweaks/content/blocks/JapaneseCedarLamp/JapaneseCedarLampBlock.java
@@ -1,8 +1,12 @@
 package uk.tfindustries.blueaerotweaks.content.blocks.JapaneseCedarLamp;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -26,6 +30,39 @@ public class JapaneseCedarLampBlock extends Block {
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    protected void neighborChanged(BlockState state, Level level, BlockPos pos, Block block, BlockPos fromPos, boolean isMoving) {
+        if (!level.isClientSide) {
+            boolean flag = (Boolean)state.getValue(CLICKED);
+            if (flag != level.hasNeighborSignal(pos)) {
+                if (flag) {
+                    level.scheduleTick(pos, this, 4);
+                } else {
+                    level.setBlock(pos, (BlockState)state.cycle(CLICKED), 2);
+                }
+            }
+        }
+
+    }
+
+    @Override
+    protected void tick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
+        if ((Boolean)state.getValue(CLICKED) && !level.hasNeighborSignal(pos)) {
+            level.setBlock(pos, (BlockState)state.cycle(CLICKED), 2);
+        }
+
+    }
+
+    @Override
+    public boolean canConnectRedstone(BlockState state, BlockGetter level, BlockPos pos, Direction side) {
+        // Return true for the side where you want redstone to point/connect
+        if ((side == Direction.NORTH) || (side == Direction.SOUTH) || (side == Direction.WEST) || (side == Direction.EAST)) {
+            return true;
+        }
+
+        return false;
     }
 
     @Override
--- src/main/java/uk/tfindustries/blueaerotweaks/registries/BlueBlocks.java
+++ src/main/java/uk/tfindustries/blueaerotweaks/registries/BlueBlocks.java
@@ -41,63 +41,63 @@ public class BlueBlocks {
     //Japanese cedar wood set
     public static final DeferredBlock<Block> JAPANESE_CEDAR_WOOD = registerBlock("japanese_cedar_wood",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(2f).requiresCorrectToolForDrops()));
+                    .strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<Block> STRIPPED_JAPANESE_CEDAR_WOOD = registerBlock("stripped_japanese_cedar_wood",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(2f).requiresCorrectToolForDrops()));
+                    .strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<RotatedPillarBlock> JAPANESE_CEDAR_LOG = registerBlock("japanese_cedar_log",
             () -> new RotatedPillarBlock(BlockBehaviour.Properties.of()
-                    .strength(2f).requiresCorrectToolForDrops()));
+                    .strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<RotatedPillarBlock> STRIPPED_JAPANESE_CEDAR_LOG = registerBlock("stripped_japanese_cedar_log",
             () -> new RotatedPillarBlock(BlockBehaviour.Properties.of()
-                    .strength(2f).requiresCorrectToolForDrops()));
+                    .strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<Block> JAPANESE_CEDAR_PLANKS = registerBlock("japanese_cedar_planks",
             () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(2f).requiresCorrectToolForDrops()));
+                    .strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
 
     public static final DeferredBlock<StairBlock> JAPANESE_CEDAR_STAIRS = registerBlock("japanese_cedar_stairs",
             () -> new StairBlock(BlueBlocks.JAPANESE_CEDAR_PLANKS.get().defaultBlockState(),
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
 
     public static final DeferredBlock<SlabBlock> JAPANESE_CEDAR_SLAB = registerBlock("japanese_cedar_slab",
-            () -> new SlabBlock(BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+            () -> new SlabBlock(BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<PressurePlateBlock> JAPANESE_CEDAR_PRESSURE_PLATE = registerBlock("japanese_cedar_pressure_plate",
             () -> new PressurePlateBlock(BlockSetType.OAK,
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<ButtonBlock> JAPANESE_CEDAR_BUTTON = registerBlock("japanese_cedar_button",
             () -> new ButtonBlock(BlockSetType.OAK, 20,
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops().noCollission()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops().noCollission()));
 
     public static final DeferredBlock<FenceBlock> JAPANESE_CEDAR_FENCE = registerBlock("japanese_cedar_fence",
-            () -> new FenceBlock(BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+            () -> new FenceBlock(BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<FenceGateBlock> JAPANESE_CEDAR_FENCE_GATE = registerBlock("japanese_cedar_fence_gate",
             () -> new FenceGateBlock(WoodType.OAK,
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<WallBlock> JAPANESE_CEDAR_WALL = registerBlock("japanese_cedar_wall",
             () -> new WallBlock(
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops()));
 
     public static final DeferredBlock<DoorBlock> JAPANESE_CEDAR_DOOR = registerBlock("japanese_cedar_door",
             () -> new DoorBlock(BlockSetType.OAK,
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops().noOcclusion()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops().noOcclusion()));
 
     public static final DeferredBlock<TrapDoorBlock> JAPANESE_CEDAR_TRAPDOOR = registerBlock("japanese_cedar_trapdoor",
             () -> new TrapDoorBlock(BlockSetType.OAK,
-                    BlockBehaviour.Properties.of().strength(2f).requiresCorrectToolForDrops().noOcclusion()));
+                    BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD).requiresCorrectToolForDrops().noOcclusion()));
 
 
     public static final DeferredBlock<Block> JAPANESE_CEDAR_LAMP = registerBlock("japanese_cedar_lamp",
-            () -> new JapaneseCedarLampBlock(BlockBehaviour.Properties.of().strength(2f)
+            () -> new JapaneseCedarLampBlock(BlockBehaviour.Properties.of().strength(2f).sound(SoundType.WOOD)
                     .requiresCorrectToolForDrops().lightLevel(state -> state.getValue(JapaneseCedarLampBlock.CLICKED) ? 15 : 0)));
 
 
